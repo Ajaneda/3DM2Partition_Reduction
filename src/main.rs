6f769772c3bde009815e1c510d288f939f979481
alu0101211770 <alu0101211770@ull.edu.es>
2021-12-17 21:06:04 +0000

    implemented TDM to dot file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ron::de;
 use ron::ser::{self, PrettyConfig};
 use std::fs::File;
 use std::path::Path;
+use std::io::Write;
 use clap::{Arg, App, crate_authors};
 
 mod partition;
@@ -26,18 +27,29 @@ fn main() {
                  .takes_value(true)
                  .required(true)
                  .help("The output file that will contain the Partition instance"))
+        .arg(Arg::with_name("dot file")
+                 .short("d")
+                 .long("dot")
+                 .takes_value(true)
+                 .help("The output file used to convert the TDM instance into a dot file format"))
         .get_matches();
 
     let input_file = matches.value_of("input file").unwrap();
-    let output_file = matches.value_of("output file").unwrap_or("input.txt");
+    let output_file = matches.value_of("output file").unwrap();
+    let dot_file = matches.value_of("dot file").unwrap_or("dot-files/output.dot");
     let verbose = matches.is_present("v");
 
     let path = Path::new(input_file);
     let file = File::open(path).expect("Could not open input file");
     let instance: TDM = de::from_reader(file).unwrap();
+    
+    let dot_path = Path::new(dot_file);
+    let dot_result = instance.to_dot();
+    let mut output = File::create(dot_path).expect("Could not create file for dot output");
+    write!(output, "{}", dot_result).expect("Could not write the dot file");
 
     let write_path = Path::new(output_file);
-    let write_file = File::create(write_path).expect("Could not create file");
+    let write_file = File::create(write_path).expect("Could not create file for the Partition instance");
     let config = PrettyConfig::new();
     let partition = reduction::tdm_to_partition(&instance, verbose);
     ser::to_writer_pretty(write_file, &partition, config)
--- src/tdm.rs
+++ src/tdm.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TDM {
     cardinality: usize,
@@ -14,5 +15,41 @@ impl TDM {
     pub fn get_m(&self) -> &Vec<(usize, usize, usize)> {
         &self.m
     }
+
+    pub fn to_dot(&self) -> String {
+        let mut result = String::from("digraph G {\nrankdir = LR;");
+        let set_names = ["w", "x", "y"];
+        for (index, set_name) in set_names.iter().enumerate() {
+            result.push_str(&format!(
+                "{}\n",
+                TDM::subgraph(set_name, self.cardinality, index)
+            ));
+        }
+        for tuple in self.m.iter() {
+            result.push_str(&format!(
+                "{}{} -> {}{} -> {}{} [color=\"#FA5ABD\"];\n", // TODO generate random colors
+                set_names[0], tuple.0, set_names[1], tuple.1, set_names[2], tuple.2
+            ));
+        }
+        result.push_str("}");
+        result
+    }
+
+    fn subgraph(current_set: &str, cardinality: usize, index: usize) -> String {
+        let mut subragph_created: String = format!("subgraph cluster_{} {{\n", index);
+        subragph_created.push_str("node [style=filled];\n");
+        for i in 1..=cardinality {
+            subragph_created.push_str(&format!("{}{}", current_set, i));
+            if i == cardinality {
+                subragph_created += ";\n";
+            } else {
+                subragph_created += " ";
+            }
+        }
+        subragph_created.push_str(&format!(
+            "label = \"{}\";\n}}\n",
+            current_set.to_uppercase()
+        ));
+        subragph_created
+    }
 }
-// TODO getters
